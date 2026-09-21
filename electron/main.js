5f7e4fb97a31b3da563126e9c3659f26cba2194a
youteachtk <youteach.tk@gmail.com>
2026-09-21 08:03:40 -0600

    Remove legacy OAuth file picker dependency

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, dialog, shell, safeStorage, screen } = require('electron');
+const { app, BrowserWindow, ipcMain, shell, safeStorage, screen } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
